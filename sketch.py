e6793ba6b905cd99878860e8998075a160a3805f
sanikarajgude2002 <115875516+sanikarajgude2002@users.noreply.github.com>
2022-10-18 19:32:23 +0530

    Update sketch.py

--- sketch.py
+++ sketch.py
@@ -45,3 +45,13 @@ r = dodge(blur, gray)
 
 
 cv2.imwrite('4.png', r)
+
+image = cv2.imread('4.jpeg')  # loads an image from the specified file
+# convert an image from one color space to another
+gray_img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+invert = cv2.bitwise_not(gray_img)  # helps in masking of the image
+# sharp edges in images are smoothed while minimizing too much blurring
+blur = cv2.GaussianBlur(invert, (21, 21), 0)
+invertedblur = cv2.bitwise_not(blur)
+sketch = cv2.divide(gray_img, invertedblur, scale=256.0)
+cv2.imwrite("sketch.png", sketch)  # converted image is saved as mentioned name
